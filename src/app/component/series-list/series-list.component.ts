0025497c5ae11e17e16e6a0ca01f663cafc27f87
ILC-IT <71129685+ILC-IT@users.noreply.github.com>
2021-02-18 15:42:12 +0100

    ordenar toogle series ok

--- src/app/component/series-list/series-list.component.ts
+++ src/app/component/series-list/series-list.component.ts
@@ -8,10 +8,38 @@ import series from '../../../assets/series.json';
 })
 export class SeriesListComponent implements OnInit {
 
+  private sortJSON(data, key, orden) {
+    return data.sort(function (a, b) {
+        var x = a[key],
+        y = b[key];
+
+        if (orden === 'asc') {
+            return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+        }
+
+        if (orden === 'desc') {
+            return ((x > y) ? -1 : ((x < y) ? 1 : 0));
+        }
+    });
+  }
+
   seriesData: any;
+  copia: any;
+
   constructor() {
+    this.copia = JSON.parse(JSON.stringify(series));
     this.seriesData = series;
    }
+
+  flag: boolean = false;
+  private ordenar(){
+    this.flag = !this.flag;
+    if (this.flag) {
+      this.seriesData = this.sortJSON(this.copia, 'name', 'asc');
+    }else{
+      this.seriesData = series;
+    }
+  }
 
   ngOnInit() {
   }
